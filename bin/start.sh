e8e41786be10a9f10f0befe098433e317936eb03
mhightower83 <27247790+mhightower83@users.noreply.github.com>
2017-12-24 18:12:46 -0800

    Update start.sh
    Removed deprecated debug test.

--- bin/start.sh
+++ bin/start.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 #
-interface=eth0
 logfile=/opt/habridge/log/habridge.log
 echo "`date +"%b %d %T"` -------- Started $0 log ---------">$logfile
-if [ -f "/var/run/dhcpcd/ipv4_interface/$interface" ]; then
-  echo "`date +"%b %d %T"` $interface: `cat /var/run/dhcpcd/ipv4_interface/$interface`">>$logfile
-else
-  echo "`date +"%b %d %T"` WARNING: dhcpcd does not appear to have completed.">>$logfile
-fi
 cd /opt/habridge
 echo "`date +"%b %d %T"` whoami `whoami`">>$logfile
-ifconfig >>$logfile
 chmod 666 $logfile
 nohup authbind --deep java \
       -jar \
